5be5f8773787d524e1e6f9247f94ef8d20024fa9
Markosaur <mark3pace@gmail.com>
2019-08-06 16:40:12 -0600

    Completed app-5

---
+++ app-4/src/Login.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+class Login extends Component{
+    constructor(){
+        super();
+        this.state = {
+            username: '',
+            password: '',
+        }
+    }
+    updateUsername=(value)=>{
+        this.setState({username: value})
+    }
+    updatePassword=(value)=>{
+        this.setState({password: value})
+    }
+    updateLogin=()=>{
+        alert(`username: ${this.state.username} password: ${this.state.password}`);
+    }
+    render(){
+        return(
+            <div>
+                <input onChange ={(e) => this.updateUsername(e.target.value)} value={this.state.username}/>
+                <input onChange ={(e) => this.updatePassword(e.target.value)} value ={this.state.password}/>
+                <button onClick = {this.updateLogin}>Login</button>
+            </div>
+        )
+    }
+}
+
+export default Login
--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Image from "./Image";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className = "App">
+        <Image url="https://media-cdn.tripadvisor.com/media/photo-s/0b/4f/52/63/happy-giraffe.jpg"/>
       </div>
     );
   }
 }
-
 export default App;
